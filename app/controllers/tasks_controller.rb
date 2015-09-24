f9035e6f4a6e129f230eddce25a99f6ac587b3f2
Kesin <kaixuanguiqu@gmail.com>
2015-09-24 21:47:36 +0800

    add list_id to permit params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -69,6 +69,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:title, :description, :status)
+      params.require(:task).permit(:title, :description, :status, :list_id)
     end
 end
