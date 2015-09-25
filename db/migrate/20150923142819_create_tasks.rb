379c3d0fa5c60d841fe9913f492d80f3bb9a0a80
Kesin <kaixuanguiqu@gmail.com>
2015-09-25 23:13:44 +0800

    change task status default to 1

--- db/migrate/20150923142819_create_tasks.rb
+++ db/migrate/20150923142819_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration
     create_table :tasks do |t|
       t.text :title
       t.text :description
-      t.integer :status
+      t.integer :status, :default => 1
       t.integer :list_id
 
       t.timestamps null: false
