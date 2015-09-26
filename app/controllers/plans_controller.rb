d0a070765969b28206aaa05262a1ee2095336e79
Kesin <kaixuanguiqu@gmail.com>
2015-09-26 14:41:04 +0800

    ensure header

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -28,7 +28,7 @@ class PlansController < ApplicationController
 
     respond_to do |format|
       if @plan.save
-        format.html { redirect_to @plan, notice: 'Plan was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Plan was successfully created.' }
         format.json { render :show, status: :created, location: @plan }
       else
         format.html { render :new }
