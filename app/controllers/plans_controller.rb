ad4e7a5e9fad7d8fd8172d73ba8010bee912a255
Kesin <kaixuanguiqu@gmail.com>
2015-09-26 13:10:15 +0800

    seperate header to individual file

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -42,7 +42,7 @@ class PlansController < ApplicationController
   def update
     respond_to do |format|
       if @plan.update(plan_params)
-        format.html { redirect_to @plan, notice: 'Plan was successfully updated.' }
+        format.html { redirect_to root_url, notice: 'Plan was successfully updated.' }
         format.json { render :show, status: :ok, location: @plan }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class PlansController < ApplicationController
   def destroy
     @plan.destroy
     respond_to do |format|
-      format.html { redirect_to plans_url, notice: 'Plan was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Plan was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -2,9 +2,10 @@ module PlansHelper
 
   # default color_tag
   # TODO: User can create different color_tag to choose
+
   def color_tag_class(color_tag)
     color_id = color_tag.to_i - 1
-    color_arr = ['gray','blue','green','red','yellow','qing'] # add expect 'gray' at the begin to advoid problem
+    color_arr = ['gray','blue','green','red','yellow','qing']
     "bs-callout-#{color_arr[color_id]}"
   end
 
