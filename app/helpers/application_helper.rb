da26a6fbb0d3f5c0cf1149863b3676a6a58448ad
Zoker <kaixuanguiqu@gmail.com>
2016-07-26 21:22:39 +0800

    add title helper for title and add title for devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,8 @@ module ApplicationHelper
     [['灰色(Grey)','0'],['蓝色(Blue)','1'],['绿色(Green)','2'],['红色(Red)','3'],['黄色(Yellow)','4'],['青色(Qing)','5']]
   end
 
+  def title(title)
+    content_for(:title, title)
+  end
+
 end
