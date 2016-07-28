ae6ecc8945d48c3b387238c7833d836de1bffa00
Zoker <kaixuanguiqu@gmail.com>
2016-07-28 22:11:20 +0800

    optimize index js seperate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require bootstrap.min
+//= require bootstrap.js
+//= require plans
+//= require lists
+//= require tasks
---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,4 @@
+//= require jquery.easing.min
+//= require scrollreveal.min
+//= require jquery.magnific-popup.min
+//= require creative
