5b9102b41992e70a10e878f726012a4bada239c3
Zoker <kaixuanguiqu@gmail.com>
2016-07-28 21:37:30 +0800

    remove old index code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,27 +5,4 @@ class HomeController < ApplicationController
   def index
   end
 
-  private
-
-  # show devise form in index page
-  # refer: https://pupeno.com/2016/04/26/show-a-devise-log-in-or-sign-up-forms-in-another-page/
-  def resource_name
-    :user
-  end
-  helper_method :resource_name
-
-  def resource
-    @resource ||= User.new
-  end
-  helper_method :resource
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
-  helper_method :devise_mapping
-
-  def resource_class
-    User
-  end
-  helper_method :resource_class
 end
