3c8802691f5ba4e9573a3f7c259ae3ed94c95f14
Zoker <kaixuanguiqu@gmail.com>
2016-08-18 22:03:29 +0800

    list controller translate

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,7 +16,7 @@ class ListsController < ApplicationController
 
     respond_to do |format|
       if @list.save
-        format.html { redirect_to @plan, notice: 'List was successfully created.' }
+        format.html { redirect_to @plan, notice: '列表已经成功创建!' }
         format.json { render :show, status: :created, location: @list }
       else
         format.html { render :new }
@@ -30,7 +30,7 @@ class ListsController < ApplicationController
   def update
     respond_to do |format|
       if @list.update(list_params)
-        format.html { redirect_to @list, notice: 'List was successfully updated.' }
+        format.html { redirect_to @list, notice: '列表已经成功更新!' }
         format.json { render :show, status: :ok, location: @list }
       else
         format.html { render :edit }
@@ -44,7 +44,7 @@ class ListsController < ApplicationController
   def destroy
     @list.destroy
     respond_to do |format|
-      format.html { redirect_to lists_url, notice: 'List was successfully destroyed.' }
+      format.html { redirect_to lists_url, notice: '列表删除成功!' }
       format.json { head :no_content }
     end
   end
