52fa025549d6dfa4163b325ebea648323f11c826
Zoker <kaixuanguiqu@gmail.com>
2016-10-04 14:31:11 +0800

    add create plan function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   end
 
   def options_for_color
-    [['灰色(Grey)','0'],['蓝色(Blue)','1'],['绿色(Green)','2'],['红色(Red)','3'],['黄色(Yellow)','4'],['青色(Qing)','5']]
+    [['灰色(Gray)','0'],['蓝色(Blue)','1'],['绿色(Green)','2'],['红色(Red)','3'],['黄色(Yellow)','4'],['青色(Qing)','5']]
   end
 
 
